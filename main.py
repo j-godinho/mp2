a7a1072f8aaa15cc0afd1e4864d1b682aa039985
Joao Godinho <joaogodinho_4@hotmail.com>
2016-11-04 00:34:06 +0000

    working on the normalization

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 
+import re
+def normalize_file():
+	path = 'corpora/training/AlmadaNegreiros/pg22615.txt'
+	f = open(path, 'r')
+	read_data = f.read()
+	f.close()
+	tokens = re.findall(r"\w+(?:[-']\w+)*|'|[-.(]+|\S\w*", read_data)
+	
+
+	f=open(path, 'w')
+	for token in tokens:
+		print token
+		f.write(token+" ")
+
+	f.close()
+
 def main():
-    print 'Hello World'
+    normalize_file()
 
 
 if __name__ == '__main__':
