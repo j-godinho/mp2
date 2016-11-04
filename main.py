9836c287989c14d867310234f1ea60ede57bf6bf
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-11-04 01:28:12 +0000

    added add one smoothing and counting of the number of tokens

--- main.py
+++ main.py
@@ -2,6 +2,19 @@ from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
 import re
 
+def add_one_smoothing(d, n):
+    ds = dict ()
+    v = len(d.keys())
+    for key in d.keys():
+        ds[key] = float(d[key] + 1) * ( float(n) / (n + v) )
+    return ds
+
+def calc_number_of_tokens(counts):
+    n = 0
+    for i in counts.values():
+        n += i
+    return n
+
 def calc_n_grams(text, lower, upper):
     vectorizer = CountVectorizer(ngram_range=(lower,upper))
     analyze = vectorizer.build_analyzer()
@@ -16,7 +29,13 @@ def space_normalization(path):
 
 
 def main():
-    print space_normalization('corpora/training/AlmadaNegreiros/pg22615.txt')
+    text = space_normalization('corpora/training/AlmadaNegreiros/pg22730.txt')
+    counts = calc_n_grams(text, 1, 2)
+    n_tokens = calc_number_of_tokens(counts)
+    counts_smoothed = add_one_smoothing(counts, n_tokens)
+    for key in counts.keys():
+        print '{}\t\t\t{}\t\t\t{}'.format(key.encode('utf-8'), counts[key], counts_smoothed[key])
+
 
 if __name__ == '__main__':
     main()
