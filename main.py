9ef75b392441802ae94abd60dda0c2969509b071
Joao Godinho <joaogodinho_4@hotmail.com>
2016-11-04 16:56:58 +0000

    average on the way

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
 import re
+import io
+from nltk import word_tokenize
+def average_words_sentence(path):
+	#.!?;
+	#...
+
+	f = io.open(path, 'r', encoding='utf8')
+	file_data = f.read()
+	tokens = word_tokenize(file_data)
+
+	end_symbols = ['!', '?', '...', ';', '.']
+	sentences = 0
+	for token in tokens:
+		if(token in end_symbols):
+			sentences += 1
+
+	print "average:", float(len(tokens)-sentences)/sentences
+
+
+
 
 def add_one_smoothing(d, n):
     ds = dict ()
@@ -23,12 +43,13 @@ def calc_n_grams(text, lower, upper):
     return dict ( Counter(ngrams) )
 
 def main():
-    text = space_normalization('corpora/training/AlmadaNegreiros/pg22730.txt')
-    counts = calc_n_grams(text, 1, 2)
-    n_tokens = calc_number_of_tokens(counts)
-    counts_smoothed = add_one_smoothing(counts, n_tokens)
-    for key in counts.keys():
-        print '{}\t\t\t{}\t\t\t{}'.format(key.encode('utf-8'), counts[key], counts_smoothed[key])
+    #text = space_normalization('corpora/training/AlmadaNegreiros/pg22730.txt')
+    #counts = calc_n_grams(text, 1, 2)
+    #n_tokens = calc_number_of_tokens(counts)
+    #counts_smoothed = add_one_smoothing(counts, n_tokens)
+    #for key in counts.keys():
+    #    print '{}\t\t\t{}\t\t\t{}'.format(key.encode('utf-8'), counts[key], counts_smoothed[key])
+    average_words_sentence('output/AlmadaNegreiros/AlmadaNegreiros.txt')
 
 if __name__ == '__main__':
     main()
