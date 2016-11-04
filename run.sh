ea8e378218b9330247ddcfd249d7c7dbbfa38d08
Joao Godinho <joaogodinho_4@hotmail.com>
2016-11-04 15:19:28 +0000

    added run.sh now creates directories of output

---
+++ run.sh
@@ -0,0 +1,7 @@
+echo 'creating directories'
+mkdir -p output/AlmadaNegreiros
+mkdir -p output/LuisaMarquesSilva
+mkdir -p output/JoseSaramago
+mkdir -p output/JoseRodriguesSantos
+mkdir -p output/EcaDeQueiros
+mkdir -p output/CamiloCasteloBranco
