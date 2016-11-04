d6e653ec2a8871015a723e995d2ea5f9d0c36d89
Joao Godinho <joaogodinho_4@hotmail.com>
2016-11-04 18:08:14 +0000

    average words per sentence already classifying each writer

--- main.py
+++ main.py
@@ -3,22 +3,7 @@ from collections import Counter
 import re
 import io
 from nltk import word_tokenize
-def average_words_sentence(path):
-	#.!?;
-	#...
-
-	f = io.open(path, 'r', encoding='utf8')
-	file_data = f.read()
-	tokens = word_tokenize(file_data)
-
-	end_symbols = ['!', '?', '...', ';', '.']
-	sentences = 0
-	for token in tokens:
-		if(token in end_symbols):
-			sentences += 1
-
-	print "average:", float(len(tokens)-sentences)/sentences
-
+import glob
 
 
 
@@ -42,14 +27,42 @@ def calc_n_grams(text, lower, upper):
 
     return dict ( Counter(ngrams) )
 
+
+def average_words_sentence(path, writer):
+	#.!?;
+	#...
+
+	f = io.open(path, 'r', encoding='utf8')
+	file_data = f.read()
+	tokens = word_tokenize(file_data)
+
+	end_symbols = ['!', '?', '...', ';', '.']
+	sentences = 0
+	for token in tokens:
+		if(token in end_symbols):
+			sentences += 1
+
+	return float(len(tokens)-sentences)/sentences
+
+def analyze_writers():
+	writers = ["AlmadaNegreiros", "EcaDeQueiros", "JoseRodriguesSantos", "CamiloCasteloBranco", "JoseSaramago", "LuisaMarquesSilva"]
+	writers_dict = dict()
+	for i in range(len(writers)):
+		print "Analyzing average words per sentence of:", writers[i]
+		path = 'output/' + writers[i]+'/' + writers[i] + '.txt'
+		writers_dict[writers[i]] = average_words_sentence(path, writers[i])
+	return writers_dict
+
+def analyze_files():
+	writers_dict = analyze_writers()
+
+
+	
+
 def main():
-    #text = space_normalization('corpora/training/AlmadaNegreiros/pg22730.txt')
-    #counts = calc_n_grams(text, 1, 2)
-    #n_tokens = calc_number_of_tokens(counts)
-    #counts_smoothed = add_one_smoothing(counts, n_tokens)
-    #for key in counts.keys():
-    #    print '{}\t\t\t{}\t\t\t{}'.format(key.encode('utf-8'), counts[key], counts_smoothed[key])
-    average_words_sentence('output/AlmadaNegreiros/AlmadaNegreiros.txt')
+	analyze_files()
+
 
 if __name__ == '__main__':
     main()
+
