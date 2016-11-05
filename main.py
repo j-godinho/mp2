0b7f65088d91ed00cd2670a4e42d6315c3f7ae02
Bernardo Casaleiro <bernardocasaleiro@gmail.com>
2016-11-05 18:00:57 +0000

    added reading of all files. added part of the training (still missing the calculation of the probabilities). added output of the unigrams and bigrams and the smoothed version.

--- main.py
+++ main.py
@@ -1,13 +1,39 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from collections import Counter
-import re
+import io
 
-def add_one_smoothing(d, n):
+class writer(object):
+
+    def __init__(self, name):
+        self.name = name
+
+class test_subject(object):
+
+    def __init__(self, test_length, index):
+        self.test_length = test_length
+        self.index = index
+
+def read_file(path):
+    f = io.open(path, 'r', encoding='utf8')
+    text = f.read()
+    f.close()
+
+    return text
+
+def add_one_smoothing(count, n, v):
+    return float(count + 1) * ( float(n) / (n + v) )
+
+def smoothing_unigrams(unigrams, n, v):
     ds = dict ()
-    v = len(d.keys())
-    for key in d.keys():
-        ds[key] = float(d[key] + 1) * ( float(n) / (n + v) )
+    for key in unigrams.keys():
+        ds[key] = add_one_smoothing( unigrams[key], n, v )
     return ds
+
+def smoothing_bigrams(unigrams, bigrams, v):
+    ds = dict ()
+    for key in bigrams.keys():
+        n = unigrams[ key.split()[1] ]
+        ds[key] = add_one_smoothing( bigrams[key], n, v )
 
 def calc_number_of_tokens(counts):
     n = 0
@@ -22,13 +48,84 @@ def calc_n_grams(text, lower, upper):
 
     return dict ( Counter(ngrams) )
 
+def output_with_smoothing(path, writer):
+    f_u = io.open(path + 'smoothed_unigrams.txt', 'w', encoding='utf8')
+    for key in w.s_unigrams.keys():
+        f_u.write( key + '\t' + str(w.s_unigrams[key]) + '\n')
+    f_u.close()
+
+    f_b = io.open(path + 'smoothed_bigrams.txt', 'w', encoding='utf8')
+    for key in w.s_bigrams.keys():
+        f_b.write( key + '\t' + str(w.s_bigrams[key]) + '\n')
+    f_b.close()
+
+def output_without_smoothing(path, w):
+    f_u = io.open(path + 'unigrams.txt', 'w', encoding='utf8')
+    for key in w.unigrams.keys():
+        f_u.write( key + '\t' + str(w.unigrams[key]) + '\n')
+    f_u.close()
+
+    f_b = io.open(path + 'bigrams.txt', 'w', encoding='utf8')
+    for key in w.bigrams.keys():
+        f_b.write( key + '\t' + str(w.bigrams[key]) + '\n')
+    f_b.close()
+
+def training():
+    writer_name = ["AlmadaNegreiros", "EcaDeQueiros", "JoseRodriguesSantos", "CamiloCasteloBranco", "JoseSaramago", "LuisaMarquesSilva"]
+    writers = []
+
+    for w in writer_name:
+        path = 'output/' + w + '/'
+        c_writer = writer(w)
+        text = read_file(path + w + '.txt')
+
+        print '[' + w + ']'
+
+        print 'unigrams'
+        c_writer.unigrams = calc_n_grams(text, 1, 1)
+        print 'bigrams'
+        c_writer.bigrams = calc_n_grams(text, 2, 2)
+        print 'N'
+        c_writer.n = calc_number_of_tokens(c_writer.unigrams)
+        print 'V'
+        c_writer.v = len(c_writer.unigrams.keys())
+        print 'smoothed unigrams'
+        c_writer.s_unigrams = smoothing_unigrams(c_writer.unigrams, c_writer.n, c_writer.v)
+        print 'smoothed bigrams'
+        c_writer.s_bigrams = smoothing_bigrams(c_writer.unigrams, c_writer.bigrams, c_writer.v)
+
+        print 'writing files'
+        output_without_smoothing(path, c_writer)
+        output_with_smoothing(path, c_writer)
+
+        writers.append( c_writer )
+
+        print ' '
+
+    return writers
+
+def testing(testing_length):
+    tests = []
+    for t in testing_length:
+        path = 'output/test/' + t + '/'
+        for i in range(6):
+            ts = test_subject(t, i)
+            text = read_file(path + str(i) + '.txt')
+
+            print '[' + t + ' test ' + str(i) + ']'
+
+            print  str(n) + '-grams to ' + str(m) +
+            ts.unigrams = calc_n_grams(text, 1, 1)
+
+            print ' '
+    return tests
+
 def main():
-    text = space_normalization('corpora/training/AlmadaNegreiros/pg22730.txt')
-    counts = calc_n_grams(text, 1, 2)
-    n_tokens = calc_number_of_tokens(counts)
-    counts_smoothed = add_one_smoothing(counts, n_tokens)
-    for key in counts.keys():
-        print '{}\t\t\t{}\t\t\t{}'.format(key.encode('utf-8'), counts[key], counts_smoothed[key])
+    testing_length = ['500Palavras', '1000Palavras']
+
+    training()
+    testing(['500Palavras'])
+
 
 if __name__ == '__main__':
     main()
